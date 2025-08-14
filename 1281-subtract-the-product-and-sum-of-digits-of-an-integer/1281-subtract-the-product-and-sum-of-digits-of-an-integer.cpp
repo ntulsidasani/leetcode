cf4faefb9068e3bc38bfc5b14482f4196dd015b5
ntulsidasani <nikitatulsidasani@gmail.com>
2025-08-14 21:16:27 +0530

    .cpp 08-14-2025 Easy

---
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int product=1,sum=0;
+        while(n>0){
+            int x=n%10;
+            n=n/10;
+            product*=x;
+            sum+=x;
+        }
+        return (product-sum);
+    }
+};
