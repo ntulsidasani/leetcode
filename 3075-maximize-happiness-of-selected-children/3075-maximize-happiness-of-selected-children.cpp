5096dd4d28498c3a267f45a73724f656ea732c84
Nikita Tulsidasani <nikitatulsidasani@gmail.com>
2025-12-25 16:03:08 +0530

    .cpp 12-25-2025 Medium

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        long long ans=0;
+        sort(happiness.begin(),happiness.end(),greater<>());
+        for(int i=0;i<k;i++){
+            ans+=max(happiness[i]-i,0);
+        }
+        return ans;
+    }
+};
