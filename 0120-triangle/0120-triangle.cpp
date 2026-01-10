6356cc638e1e0dd96106e9244991708bc38efa5b
Nikita Tulsidasani <nikitatulsidasani@gmail.com>
2026-01-10 23:41:25 +0530

    .cpp 01-10-2026 Medium

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        dp[0][0]=triangle[0][0];
+        for(int i=1;i<n;i++){
+            for(int j=0;j<triangle[i].size();j++){
+                dp[i][j]=triangle[i][j];
+                if(j==0){
+                    dp[i][j]+=dp[i-1][j];
+                }else if(i==j){
+                    dp[i][j]+=dp[i-1][j-1];
+                }else {
+                    dp[i][j]+=min(dp[i-1][j],dp[i-1][j-1]);
+                }
+            }
+        }
+        return *min_element(dp[n-1].begin(),dp[n-1].end());
+
+    }
+};
