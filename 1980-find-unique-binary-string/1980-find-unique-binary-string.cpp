13fa6d15f30db1a5e133286aef27f8c9ab796490
Nikita Tulsidasani <nikitatulsidasani@gmail.com>
2026-03-08 13:59:38 +0530

    .cpp 03-08-2026 Medium

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    void solve(set<string> &ans,int n,string curr){
+    string solve(unordered_map<string,int> &ans,int n,string curr){
         if(curr.size()==n){
-            ans.insert(curr);
-            return;
+            if(!ans.count(curr))return curr;
+            return "";
         }
-        solve(ans,n,curr+'0');
-        solve(ans,n,curr+'1');
+        string left=solve(ans,n,curr+'0');
+        if(!left.empty())return left;
+        
+        string right=solve(ans,n,curr+'1');
+        return right;
     }
     string findDifferentBinaryString(vector<string>& nums) {
-        set<string>A;
-        solve(A,nums[0].size(),"");
+        
         unordered_map<string,int>freq;
         for(auto &a:nums)freq[a]++;
-        for(auto &a:A){
-            if(!freq.count(a))return a;
-        }
-        return "";
+        string ans=solve(freq,nums[0].size(),"");
+        return ans;
     }
 };
